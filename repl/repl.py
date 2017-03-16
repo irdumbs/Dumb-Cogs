0eb930129f3a2e1d5d5ba3ae4556e42e4cc7f4e2
Chovin <ccarlson@docomopacific.com>
2017-03-16 10:27:06 +1000

    [repl] added dir/pyhelp/rtfs and rtfs to repl
    [p]dir also has search and exclude regex optionals

--- repl/repl.py
+++ repl/repl.py
@@ -11,6 +11,7 @@ from contextlib import redirect_stdout
 from __main__ import send_cmd_help
 import io
 import os
+import re
 import subprocess
 from collections import OrderedDict
 
@@ -23,6 +24,17 @@ from collections import OrderedDict
 #   * cog info / author
 #       * look in downloads if not found
 #           * mention that it's not installed
+# TODO: set repl character `
+# TODO: set pager characters
+
+
+class Source:
+    def __init__(self, cmd):
+        self.filename = inspect.getsourcefile(cmd)
+        source = inspect.getsourcelines(cmd)
+        self.line_number = source[1]
+        self.source = ''.join(source[0])
+
 
 class ReactionRemoveEvent(asyncio.Event):
     def __init__(self, emojis, author):
@@ -43,6 +55,46 @@ class REPL:
         self.output_file = "data/repl/temp_output.txt"
         self.sessions = set()
         self.reaction_remove_events = {}
+
+    def repl_format_source(self, thing):
+        """returns get_source formatted to be used in repl
+
+        rtfs originated as this alias:
+        debug (lambda cmd, bot=bot: (lambda f, out: out[0] if len(out) == 1 else (f(f,out[1:5] + (['{} more pages remaining..\njust tell them to read the actual source file man.'.format(len(out)-5)] if len(out) > 5 else [])) or out[0]))((lambda self, more: None if not more else bot.loop.create_task(bot.say('``'+'`py\n'+more.pop(0)+'``'+'`')).add_done_callback(self(self, more))), list(pagify((lambda ic, fc, pg: (lambda fcs: ic.getsourcefile(fc).split('/')[-1]+'\nline: {}'.format(fcs[1])+'``'+'`'+'\n'+'``'+'`py\n'+''.join(fcs[0]))(ic.getsourcelines(fc)))(__import__('inspect'), (cmd if not isinstance(cmd, str) else (lambda f, ms: f(f, __import__(ms.pop(0)), ms))((lambda f, prev, ms: getattr(prev, 'callback') if hasattr(prev, 'callback') else prev if not ms else f(f, getattr(prev, ms.pop(0)), ms)), cmd.split('.')) if '.' in cmd else (lambda end, cmds: end(end, cmds, bot.commands[cmds.pop(0)]).callback)((lambda end, names, cmd: cmd if not names else end(end, names, cmd.commands[names.pop(0)])), cmd.split()) ), __import__('cogs').utils.chat_formatting.pagify), delims=['\n', ' '], escape=False, shorten_by=12)) ))
+        """
+        source = self.get_source(thing)
+        msg =  source.filename.split('/')[-1] + '\n'
+        msg += 'line: {}'.format(source.line_number)
+        msg += '``'+'`\n`'+'``py\n'  # codeblock break
+        msg += source.source
+        return msg
+
+    def get_source(self, thing):
+        """returns a source object of a thing
+
+        thing may be a non-builtin module, class, method, function, traceback, frame, or code object,
+        or a space separated discord.ext.commands call,
+        or a period deliminated file/module path as used when importing
+        """
+        if isinstance(thing, str):
+            if '.' in thing:  # import
+                modules = thing.split('.')
+                def get_last_attr(prev, attrs):
+                    try:
+                        return prev.callback
+                    except AttributeError:
+                        if not attrs:
+                            return prev
+                        return get_last_attr(getattr(prev, attrs.pop(0)),
+                                                     attrs)
+                thing = get_last_attr(__import__(modules.pop(0)), modules)
+            else:  # space delimited command call
+                names = thing.split()
+                thing = self.bot.commands[names.pop(0)]
+                for name in names:
+                    thing = thing.commands[name]
+                thing = thing.callback
+        return Source(thing)
 
     def cleanup_code(self, content):
         """Automatically removes code blocks from the code."""
@@ -86,6 +138,7 @@ class REPL:
                 if open_cmd:
                     subprocess.Popen([open_cmd, self.output_file])
         else:
+
             await self.bot.send_message(msg.channel, discord_fmt.format(results))
 
     async def interactive_results(self, ctx, results, single_msg=True):
@@ -233,6 +286,67 @@ class REPL:
         except:
             return None
 
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def dir(self, ctx, thing: str,
+                  re_search: str=".*", re_exclude: str="^_"):
+        """displays the attributes of a thing
+
+        provide a second argument as a regex pattern to search for within the list
+        provide a third exclude pattern to exclude those matches from the list
+        defaults to excluding items starting with an underscore "_" """
+        # re_search = re.escape(re_search)
+        # re_exclude = re.escape(re_exclude)
+        bot = self.bot
+        message = ctx.message
+        author = message.author
+        channel = message.channel
+        server = message.server
+
+        res = repr([a for a in dir(eval(thing))
+                    if not re.search(re_exclude, a)
+                    and re.search(re_search, a)])
+        await self.print_results(ctx, res)
+
+    @commands.command(pass_context=True, aliases=['rtfh'])
+    @checks.is_owner()
+    async def pyhelp(self, ctx, *, thing: str):
+        """displays the help documentation for a python thing"""
+        bot = self.bot
+        message = ctx.message
+        author = message.author
+        channel = message.channel
+        server = message.server
+
+        stdout = io.StringIO()
+        try:
+            with redirect_stdout(stdout):
+                result = help(eval(thing))
+        except Exception as e:
+                value = stdout.getvalue()
+                fmt = '{}{}'.format(value, traceback.format_exc())
+        else:
+            value = stdout.getvalue()
+            fmt = (value or '') + ('' if result is None else result)
+        await self.print_results(ctx, fmt)
+
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def rtfs(self, ctx, *, thing: str):
+        """tries to show the source file of a thing
+
+        thing may be a non-builtin module, class, method, function, traceback, frame, or code object,
+        or if surrounded by single or double quotes,
+            a space separated discord.ext.commands call,
+            or a period deliminated file/module path as used when importing"""
+        bot = self.bot
+        message = ctx.message
+        author = message.author
+        channel = message.channel
+        server = message.server
+
+        await self.print_results(ctx, self.repl_format_source(eval(thing)))
+
     @commands.command(pass_context=True, hidden=True)
     @checks.is_owner()
     async def repl(self, ctx):
@@ -245,6 +359,7 @@ class REPL:
             'server': msg.server,
             'channel': msg.channel,
             'author': msg.author,
+            'rtfs': self.repl_format_source,
             '_': None,
         }
 
