dcc6b600681b94c9136837f27baba86be668acb0
Chovin <Chovin@users.noreply.github.com>
2017-04-22 17:46:25 +1000

    pep8
    Thanks Zephyrkul#1795

--- engine/engine.py
+++ engine/engine.py
@@ -1,17 +1,17 @@
-import discord
 from discord.ext import commands
 
 
 class Engine:
-	"""leavin you in the dust!"""
-	def __init__(self,bot):
-		self.bot = bot
+    """leavin you in the dust!"""
 
-	@commands.command(pass_context=True)
-	async def vroom(self, ctx):
-		"""You don't need no instruction manual to be free!!"""
-		await self.bot.say("VROOM VROOM!")
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def vroom(self, ctx):
+        """You don't need no instruction manual to be free!!"""
+        await self.bot.say("VROOM VROOM!")
+
 
 def setup(bot):
-	n = Engine(bot)
-	bot.add_cog(n)
+    bot.add_cog(Engine(bot))
